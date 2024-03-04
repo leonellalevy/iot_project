ac75edf23f96b5b8aa2d9ba6526320244785cce3
%username% <%email%>
2024-03-04 13:43:08 -0500

    Tested with breadboard

--- Project/LED.py
+++ Project/LED.py
@@ -1,24 +1,24 @@
 import RPi.GPIO as GPIO
 
 class LED:
-    pin = 27
-    state = False
-    
-    def __init__(self, pin, initial_state):
+    def __init__(self, pin, initial_state=False):
+        self.pin = pin
         self.state = initial_state
 
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin,GPIO.OUT,initial=state)
+        GPIO.setup(pin, GPIO.OUT, initial=self.state)
 
     def turn_on(self):
         self.state = True
+        GPIO.output(self.pin, GPIO.HIGH)
 
     def turn_off(self):
         self.state = False
+        GPIO.output(self.pin, GPIO.LOW)
 
-    def setupLEDState(self,state):
-        if(state == True):
-            GPIO.output(self.pin,1)
+    def setupLEDState(self, state):
+        if state:
+            GPIO.output(self.pin, GPIO.HIGH)
         else:
-            GPIO.output(self.pin,0)
+            GPIO.output(self.pin, GPIO.LOW)
--- Project/main.py
+++ Project/main.py
@@ -1,56 +1,53 @@
-# Import packages
-from dash import Dash, html, Input, Output, State, callback, dcc
+from dash import Dash, html, Input, Output, callback
 import dash_daq as daq
-import time
-from time import sleep
 from LED import LED
 
 # Light images
-img_light_off = 'assets/images/light_off.png'
-img_light_on = 'assets/images/light_on.png'
+img_light_off = 'assets/images/lightbulboff.png'
+img_light_on = 'assets/images/lightbulbon.png'
 
 # LED INFORMATION
-LED_PIN = 16
-led = LED(LED_PIN,False)
+LED_PIN = 27
+led = LED(LED_PIN, False)
 
 app = Dash(__name__)
 
 # Phase 1 content
-light_content =[
-        
-    html.Div(className="card-component", children=[
-
-        html.H3('LED'),
-        html.Div([
-            html.Img( src=img_light_off, id='light-img', className="feature-img" )
-        ]),
+light_content = [
+    html.Div(className="card", children=[
+        html.H2('LED'),
+        html.Img(src=img_light_off, id='light-img', className="feature-img"),
         html.Br(),
-        daq.BooleanSwitch( on=False, id='light-switch', className='dark-theme-control' ),
+        daq.BooleanSwitch(on=False, id='light-switch', className='dark-theme-control'),
     ])
-        
 ]
 
 # App layout
-app.layout = html.Div( id='layout',
-    children=[
-        
-        html.H1(style={},children=['IoT Project']),
-        html.Div(id='container', children=[
-            
-            html.Div(id='column', children=[
-
-                html.Div(id="right-container", children=[
-                    html.Div(id='light-container', children=light_content),
-                ])
-
+app.layout = html.Div(id='layout', children=[
+    html.H1('IoT Project', style={'margin-top': '20px'}),
+    html.Div(id='container', children=[
+        html.Div(id='column', children=[
+            html.Div(id="right-container", children=[
+                html.Div(id='light-container', children=light_content),
             ])
-        ]),
+        ])
+    ]),
+])
 
-    ]
+# Callback to update the LED state
+@app.callback(
+    Output('light-img', 'src'),
+    Output('light-switch', 'on'),
+    Input('light-switch', 'on'),
 )
+def update_led(on):
+    if on:
+        led.turn_on()
+        return img_light_on, True
+    else:
+        led.turn_off()
+        return img_light_off, False
 
 # Run the app
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', debug=True)
-    app.run(debug=True)
-    
+    app.run_server(debug=True)

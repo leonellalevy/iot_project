ea11070bb39dd585e16d5cc934cfde314be344d6
olajireolabode <98364070+olajireolabode@users.noreply.github.com>
2024-04-25 18:22:20 -0400

    Update mqtt_manager.py

--- iot_project-main/Project/mqtt_manager.py
+++ iot_project-main/Project/mqtt_manager.py
@@ -1,29 +1,30 @@
-import threading
 import paho.mqtt.client as mqtt
 import time
 
 class MQTTManager:
     def __init__(self, broker, port, topic):
-        self.client = mqtt.Client()
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
         self.broker = broker
         self.port = port
         self.topic = topic
         self.light_intensity = 0
+        
+        # Set the connection and message callbacks
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
+
+        # Connect and start the loop after setting the callbacks
         self.client.connect(self.broker, self.port, 60)
         self.client.loop_start()
 
     def on_connect(self, client, userdata, flags, rc):
         print("Connected to MQTT broker with result code: " + str(rc))
         client.subscribe(self.topic)
-        self.connected_event.set()
 
     def on_message(self, client, userdata, msg):
         print("Message received: " + msg.payload.decode())
         try:
-            self.light_intensity = float(msg.payload.decode())
-            #self.message_received_event.set() 
+            self.light_intensity = int(msg.payload.decode())
         except ValueError:
             print("Invalid light intensity value")
 
